04f3d9adf391325b6c100a2b14f58162a2386596
Fernando de Jesús Soto <fersoto117@gmail.com>
2017-08-09 23:12:55 -0500

    Edge must have a weight

--- MazeGenerator/MazeGenerator_pyde/Edge.py
+++ MazeGenerator/MazeGenerator_pyde/Edge.py
@@ -1,12 +1,23 @@
 # Edge class
 
+
+from random import randint
+
+
 class Edge:
-    def __init__(self, nodeA, nodeB):
-        self._a = nodeA
-        self._b = nodeB
+    def __init__(self, node_a, node_b):
+        self._a = node_a
+        self._b = node_b
+        self._weight = randint(1, 100007)
+        
+    def __cmp__(self, other):
+        return cmp(self.weight, other.weight)
         
     def a(self):
         return self._a
     
     def b(self):
         return self._b
+
+    def weight(self):
+        return self._weight
